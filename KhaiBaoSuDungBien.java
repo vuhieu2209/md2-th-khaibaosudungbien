1201b0977188e6dcfccd59b1f47e73adad4631dc
unknown <vuminhhieu22092001@gmail.com>
2023-09-13 14:33:03 +0700

    khaibaobien

---
+++ KhaiBaoSuDungBien.java
@@ -0,0 +1,14 @@
+public class KhaiBaoSuDungBien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
